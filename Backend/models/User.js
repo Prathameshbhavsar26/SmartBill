b6fa7a2d07b017aeb6361c883f07f9b7a2526588
Prathameshbhavsar26 <prathameshbhavsar26@gmail.com>
2026-08-04 12:02:51 +0530

    Update User model

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
       lowercase: true,
     },
 
-phone: {
+    phone: {
       type: String,
       required: true,
     },
@@ -47,7 +47,7 @@ phone: {
   },
   {
     timestamps: true,
-  }
+  },
 );
 
 export default mongoose.model("User", userSchema);
